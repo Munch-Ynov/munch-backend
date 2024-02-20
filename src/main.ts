e9caccb2aa659c29bbd33e69629843752f79dc6e
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2024-02-20 14:54:18 +0100

    Add middleware and dependencies for rate limiting and error handling

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swagger_config = new DocumentBuilder()
+    .setTitle('munchAPI')
+    .setDescription('The munch API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, swagger_config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
