aab6b746e4223605dc2aa4f423eeade3187b8217
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2024-03-12 12:08:30 +0100

    Refactor schema.prisma to use new models and enums

---
+++ src/data/models/auth.model.ts
@@ -0,0 +1,21 @@
+/**
+ * Model of the Auth
+ * @category Models
+ * @interface Auth
+ * @param {string} id - id of the auth : eg. abcdef123456
+ * @param {string} email - email of the auth
+ * @param {string} password - password of the auth
+ * @param {Date} createdAt - timestamp of the creation of the auth
+ * @param {Date} updatedAt - timestamp of the last update of the auth
+ * @param {Role} role - role of the auth
+ */
+interface Auth {
+  id: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+  role: Role;
+}
+
+export default Auth;
---
+++ src/data/models/restaurateur-profile.ts
@@ -0,0 +1,25 @@
+import type Restaurant from "./restaurant.model";
+
+/**
+ * Model of the RestaurateurProfile
+ * @category Models
+ * @interface RestaurateurProfile
+ * @param {string} id - id of the profile : eg. abcdef123456
+ * @param {string} avatar - avatar of the restaurateur
+ * @param {string} banner - banner of the restaurateur
+ * @param {Restaurant} restaurant - restaurant of the profile
+ * @param {string} restaurantId - id of the restaurant : eg. abcdef123456
+ * @param {Date} createdAt - timestamp of the creation of the restaurant profile
+ * @param {Date} updatedAt - timestamp of the last update of the restaurant profile
+ */
+interface RestaurateurProfile {
+  id: string;
+  avatar?: string;
+  banner?: string;
+  restaurant: Restaurant;
+  restaurantId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export default RestaurateurProfile;
