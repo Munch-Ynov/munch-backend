48f7188896c69cdb6aaa12fc520226f9c88f83d5
francois-bonnin <francois.bonnin@ynov.com>
2024-04-23 12:03:59 +0200

    fix: auth login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,54 +1,65 @@
-import { BadRequestException, Body, Controller, ForbiddenException, HttpException, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { Response } from 'express';
-import { Role } from '@/models';
-import { CreateAuthUserDto } from './dto/create-auth-user.dto';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  ForbiddenException,
+  HttpException,
+  Post,
+  Res,
+} from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { Role } from "@/models";
+import { CreateAuthUserDto } from "./dto/create-auth-user.dto";
+import { Response } from "express";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {
-  }
+  constructor(private readonly authService: AuthService) {}
 
-  @Post('login')
-  async login(res: Response,email: string, password: string) {
+  @Post("login")
+  async login(
+    @Res({ passthrough: true }) res: Response,
+    @Body() { email, password }: { email: string; password: string }
+  ) {
     if (!email || !password) {
       // http error
-      throw new HttpException('Email and password are required', 400);
+      throw new HttpException("Email and password are required", 400);
     }
 
-    try{
+    try {
       const tokens = await this.authService.login(email, password);
-      if(!tokens.accessToken || !tokens.refreshToken){
+      console.log(tokens);
+      if (!tokens.accessToken || !tokens.refreshToken) {
         // http error not authentification
-        throw new ForbiddenException('Invalid credentials');
+        throw new ForbiddenException("Invalid credentials");
       }
 
-      res.cookie('refreshToken', tokens.refreshToken, {
+      res.cookie("refreshToken", tokens.refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: "strict",
       });
 
-      return {"accessToken" : tokens.accessToken}
-    }catch(e){
+      return { accessToken: tokens.accessToken };
+    } catch (e) {
       // http error
-      throw new ForbiddenException('Invalid credentials');
+      console.error(e);
+      throw new ForbiddenException("Invalid credentials");
     }
-     
   }
 
-  @Post('register')
+  @Post("register")
   async register(@Body() createAuthUserDto: CreateAuthUserDto) {
-
-    return await this.authService.register(createAuthUserDto.email, createAuthUserDto.password, createAuthUserDto.role);
+    return await this.authService.register(
+      createAuthUserDto.email,
+      createAuthUserDto.password,
+      createAuthUserDto.role
+    );
   }
 
-  @Post('logout')
+  @Post("logout")
   async logout(res: Response) {
-    res.clearCookie('refreshToken');
-    return {message: 'Logout success'};
+    res.clearCookie("refreshToken");
+    return { message: "Logout success" };
   }
-
-  
-
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({ origin: "*" });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const swagger_config = new DocumentBuilder()
-    .setTitle('munchAPI')
-    .setDescription('The munch API description')
-    .setVersion('1.0')
-    .addTag('API')
+    .setTitle("munchAPI")
+    .setDescription("The munch API description")
+    .setVersion("1.0")
+    .addTag("API")
     .build();
   const document = SwaggerModule.createDocument(app, swagger_config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
